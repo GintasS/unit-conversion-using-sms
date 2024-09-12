ad75b07fdd4421aacd90f57caecf2945cdc7ebde
GintasS <svedgintas@gmail.com>
2024-09-11 17:48:15 -0700

    Running scheduled jobs and unit conversion from the main.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from os import environ
 from flask_routes import flask_app
+from utilities.helper import initialize_unit_conversion 
+from services.scheduler_service import schedule_jobs
+
+import logging
 
 if __name__ == '__main__':
-  flask_app.run(host='0.0.0.0', port=5000)
+  logging.basicConfig(level=logging.INFO, format="%(levelname)s %(funcName)s: %(message)s")
+  logging.info("[Unit Conversion] Starting Unit Conversion project..")
+  
+  initialize_unit_conversion()
+  schedule_jobs()
+  flask_app.run(host='0.0.0.0', port=8000)
